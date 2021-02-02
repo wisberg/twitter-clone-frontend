78b22e7a15377a7cb0e110baf8c3eefe2032e44a
wisberg <duffisberg@rogers.com>
2021-02-02 09:29:57 -0600

    commit on master

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,28 +1,32 @@
-import App from './App';
-import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from "@apollo/client";
-import { setContext } from 'apollo-link-context';
-
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  ApolloProvider,
+} from "@apollo/client";
+import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+  uri: "https://agile-castle-81869.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
-    const token = localStorage.getItem('token');
-    return{
-        headers:{
-            Authorization: token ? `Bearer ${token}` : ''
-        }
-    }
-})
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 export default (
-    <ApolloProvider client = {client}>
-        <App />
-    </ApolloProvider>
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 );
